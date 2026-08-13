32c7b74b8a68d377849a2824f39ddb612c4e5e3c
MM-MatthewSoriano <matthewsoriano763@gmail.com>
2026-08-13 13:53:37 +0800

    chore: previewing users and search added

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,101 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-
   // -----Use States-----
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
+  const [viewed, setViewed] = useState(false);
+  const [viewUser, setviewUser] = useState([]);
 
-  function searchUser(users) {
-    
-  }
+  const searchUser = users.filter((user) => {
+    const query = search.toLowerCase();
+    return (
+      user.name.toLowerCase().includes(query) ||
+      user.username.toLowerCase().includes(query) ||
+      user.email.toLowerCase().includes(query)
+    )
+  });
 
-  useEffect(() => async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    setUsers(data);
-    console.log(data);
+  const deleteUser = users.map((user) => {
+
+  })
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      setUsers(data);
+      console.log(data);
+    }
+    fetchUsers();
   },[]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <div>
       <table border="1">
         <thead>
           <tr>
-            <td colSpan="3">
-              Search <input></input>
+            <td colSpan="4">
+              Search <input type="text" value={search} onChange={(event) => {
+                setSearch(event.target.value);
+              }} />
             </td>
           </tr>
           <tr>
             <th>Name</th>
             <th>Username</th>
             <th>Email</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
-          <tr key={users.id}>
+          {searchUser.map((user) => (
+          <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.username}</td>
             <td>{user.email}</td>
+            <td>
+              <button onClick={() => {
+                setviewUser(user);
+                setViewed(true);
+                console.log(user);
+              }}>
+              View Details
+              </button>
+              <button>Delete</button>
+            </td>
           </tr>
           ))}
+          <tr>
+            <td colSpan="4">
+              {viewed ? 
+                (
+                  <div>
+                    <button onClick={() => setViewed(false)}>Close</button>
+                    <div>Name: {viewUser.name}</div>
+                    <div>Username: {viewUser.username}</div>
+                    <div>Email: {viewUser.email}</div>
+                    <div>Company: {viewUser.company.name}</div>
+                    <div>Website: <a href={viewUser.website}>{viewUser.website}</a></div>
+                  </div>
+                ) 
+                : 
+                ("Select a user to view details")
+              }
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
